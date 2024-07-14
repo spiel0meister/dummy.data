bd3f58c6efe9ca9a6fa6721ce45e5e854e0018ae
spiel0meister <soviczan7@gmail.com>
2024-07-14 02:59:38 +0200

    added fruits

---
+++ fruits.h
@@ -0,0 +1,11 @@
+#ifndef FRUITS_H_
+#define FRUITS_H_
+#include <stddef.h>
+
+const char* fruits[] = {
+    "Apples", "Apricots", "Aubergines", "Avocados", "Bananas", "Butternut squash", "Cherries", "Clementines", "Dates", "Elderberries", "Figs", "Grapes", "Guava", "Honeydew melons", "Kiwis", "Lemons", "Mangoes", "Nectarines", "Olives", "Oranges", "Pears", "Peppers", "Pineapples", "Pumpkins", "Quince", "Raisins", "Satsumas", "Strawberries", "Tomatoes", "Ugli", "Victoria plums", 
+    "Watermelons", "Zucchini", 
+};
+const size_t fruits_len = sizeof(fruits)/sizeof((fruits)[0]);
+
+#endif // FRUITS_H_
